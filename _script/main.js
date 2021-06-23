c42a67a7abe75e38874a8f4b6a886101e8d12a5f
JasonH33 <Jason.a.a.lourenco@outlook.com>
2021-06-22 23:55:16 -0300

    added icons, finished services section and menu

---
+++ _script/main.js
@@ -0,0 +1,30 @@
+// toggle menu when clicked on menu icons //
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+// toggle menu when clicked on menu items //
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+// header shadow //
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
