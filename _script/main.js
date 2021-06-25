a2369950cafc9866f0389920a5a87e7b00d0bd51
JasonH33 <Jason.a.a.lourenco@outlook.com>
2021-06-24 23:30:15 -0300

    Finished Desktop Layout

--- _script/main.js
+++ _script/main.js
@@ -1,4 +1,4 @@
-// toggle menu when clicked on menu icons
+// Opens Menu
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -8,7 +8,7 @@ for (const element of toggle) {
   })
 }
 
-// toggle menu when clicked on menu items
+// Toggle Menu
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
@@ -17,52 +17,92 @@ for (const link of links) {
   })
 }
 
-// header shadow
+// Header shadow when scrolling
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
-// Swiper testimonials slider
+// Swiper slider
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   pagination: {
     el: '.swiper-pagination'
   },
-  mousewheel: false,
-  keyboard: true
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 // ScrollReveal
 const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 800,
+  duration: 700,
   reset: true
 })
 
 scrollReveal.reveal(
-  `#home .image, #home .text, 
+  `#home .image, #home .text,
   #about .image, #about .text,
-   #services header, #services .card,
-   #testimonials header, #testimonials .testimonials,
-   #contact .text, #contact .links,
-   footer .brand, footer .social  `,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
   { interval: 100 }
 )
 
 // Back to top button
 const backToTopButton = document.querySelector('.back-to-top')
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= 2800) {
+
+function backToTop() {
+  if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
   }
+}
+
+// Active links
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+// When Scroll
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
 })
